4b4ca4e32ba69ddb176d379b53cc5beb4ef13deb
georgecma <georgema689@gmail.com>
2019-12-21 15:38:37 -0500

    working

---
+++ src/components/title-component.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class TitleComponent extends React.Component {
+	constructor (props) {
+		super (props)
+	}
+
+	render () {
+		return (
+		<>
+			<h1> {this.props.title} </h1> 
+		</>
+		)
+	}
+}
+
+// Enforces error checking 
+TitleComponent.propTypes = {
+	title:PropTypes.string.isRequired
+}
+
+export default TitleComponent 
